1dab8e94c224bd04461214a21dc36a0f4de87be4
Paras Singh <146059706+prsSng383@users.noreply.github.com>
2025-07-15 20:37:30 +0530

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -39,8 +39,8 @@ export const register = async (req , res) => {
 
    res.cookie('token', token , {
       httpOnly : true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000
    })
   
@@ -89,8 +89,8 @@ export const login = async (req, res) =>{
 
       res.cookie('token' , token , {
          httpOnly : true,
-         secure: process.env.NODE_ENV === "production",
-         sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+         secure: true,
+         sameSite:"none",
          maxAge: 7 * 24 * 60 * 60 * 1000
       })
       
